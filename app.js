1f5455d15ec0345715e84a6a08ae16310e92d73c
Inaê Rosa <inaedb.rosa@gmail.com>
2021-12-06 11:11:15 -0300

    set cookie parser, sessions and flash

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 
+// Parse JSON data
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//Set Cookie Parser, Sessions & Flash
+app.use(cookieParser('SecretStringForCookies'))
+app.use(session({
+    secret: 'SecretStringForSession',
+    cookie: {maxAge:6000}, 
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(flash());
+
+
 app.listen(3000, () => {
     console.log('running')
 })
